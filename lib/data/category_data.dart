05dd3ca952f9136311c42178b8600879b4dcf29e
Lullalynx <t80883939@gmail.com>
2025-01-09 12:56:39 +0700

    update code

---
+++ lib/data/category_data.dart
@@ -0,0 +1,6 @@
+final List<Map<String, String>> categories = [
+  {'image': 'assets/images/steak.png', 'title': 'Steak'},
+  {'image': 'assets/images/pizza.png', 'title': 'Pizza'},
+  {'image': 'assets/images/hamburger.png', 'title': 'Burgers'},
+  {'image': 'assets/images/ramen.png', 'title': 'Noodles'},
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'data/category_list.dart';
+import 'data/category_data.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,42 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: MyHomePage(title: "title"));
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,31 +36,76 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Column(
+      child: Scaffold(
+          body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
-            children: [
-              Image.asset('assets/images/user_profile.png'),
-              SizedBox(height: 8,width: 8,),
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hey Pungjung,",
-                    style: TextStyle(fontSize: 14),
-                  ),
-                  Text(
-                    "Ready to order?",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-                  )
-                ],
-              )
-            ],
+          //Profile(),
+          SizedBox(height: 16),
+          Text(
+            "Select by category",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          // cetagory_widget(),
+
+          SizedBox(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                // final category = categories[index];
+                return cetagory_widget(
+                  image: categories[index]['image']!,
+                  title: categories[index]['title']!,
+                );
+                // Text(categories[index]['title']!);
+              },
+            ),
           ),
         ],
+      )),
+    );
+  }
+}
+
+class cetagory_widget extends StatelessWidget {
+  const cetagory_widget({
+    required this.image,
+    required this.title,
+    super.key,
+  });
+
+  final String image;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            color: const Color.fromRGBO(246, 245, 243, 1)),
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              image,
+              width: 48,
+              height: 48,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
